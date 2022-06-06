73b8279dcc6aa5a65a16efa310cd25b93535568d
Abmael <abmael.cont@gmail.com>
2022-06-06 15:11:05 -0300

    dynamic array: push an element to the end of the array

---
+++ src/main/c/algorithms/datastructures/dynamic-array/dynamic-array.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Array {
+    int *arr;
+    size_t capacity;
+    size_t current;
+} Array;
+
+Array* init(Array *array, size_t size) {
+    array->arr = calloc(size, sizeof(int));
+    array->capacity = size;
+    array->current = 0;
+
+    return array;
+}
+
+int isFull(int current, int capacity) {
+    return current == capacity;
+}
+
+void freeArray(Array *array) {
+    free(array->arr);
+
+    array->arr = NULL;
+    array->capacity = 0;
+    array->current = 0;
+}
+
+Array* push(int value, Array* array) {
+    if(isFull(array->current, array->capacity)) {
+       array->capacity *= 2;
+       array->arr = realloc(array->arr, array->capacity * sizeof(int));
+    }
+
+    array->arr[array->current] = value;
+    array->current++;
+
+    return array;
+}
