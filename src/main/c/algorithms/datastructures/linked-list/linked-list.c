ec01f889454eac2eefb0cf8188260a45e65c94e7
Abmael <abmael.cont@gmail.com>
2022-06-11 13:35:46 -0300

    Create an initialized node

--- src/main/c/algorithms/datastructures/linked-list/linked-list.c
+++ src/main/c/algorithms/datastructures/linked-list/linked-list.c
@@ -5,3 +5,13 @@ typedef struct Node {
     int value;
     struct Node *next;
 } Node;
+
+// Create an initialized node.
+Node * createNode(int value) {
+    Node *node = (Node *)malloc(sizeof(Node));
+
+    node->value = value;
+    node->next = NULL;
+
+    return node;
+}
