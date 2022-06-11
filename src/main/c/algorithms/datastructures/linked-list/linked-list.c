91ac56d7a5083b6b5bf549da2c13b0af81af141d
Abmael <abmael.cont@gmail.com>
2022-06-11 13:37:20 -0300

    Check if two numbers are different

--- src/main/c/algorithms/datastructures/linked-list/linked-list.c
+++ src/main/c/algorithms/datastructures/linked-list/linked-list.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TRUE 1
+#define FALSE 0
+
 typedef struct Node {
     int value;
     struct Node *next;
@@ -14,4 +17,8 @@ Node * createNode(int value) {
     node->next = NULL;
 
     return node;
+}
+
+int isDiff(int n1, int n2) {
+    return (n1 != n2) ? TRUE : FALSE;
 }
