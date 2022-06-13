bb3dda3c1070000f4ee86eee145701d902feb8d8
Abmael <abmael.cont@gmail.com>
2022-06-13 14:43:03 -0300

    Reverse a linked list

--- src/main/c/algorithms/interview-questions/linked-list/reverse-linked-list-iterative-method.c
+++ src/main/c/algorithms/interview-questions/linked-list/reverse-linked-list-iterative-method.c
@@ -14,3 +14,25 @@ typedef struct Node {
 int isEmpty(Node * node) {
     return (node == NULL) ? TRUE : FALSE;
 }
+
+void reverse(Node **head) {
+    if(isEmpty(*head)) {
+        return;
+    }
+
+    Node* previous = NULL;
+    Node* next = NULL;
+    Node* current = *head;
+
+    while(current) {
+        next = current->next;
+
+        current->next = previous;
+
+        previous = current;
+        
+        current = next;
+    }
+
+    *head = previous;
+}
