c34fba5552485119a78b30c65beff25e32598f82
Abmael <abmael.cont@gmail.com>
2022-06-13 13:38:43 -0300

    Binary search algorithm

--- src/main/c/algorithms/search/binary-search.c
+++ src/main/c/algorithms/search/binary-search.c
@@ -6,4 +6,23 @@ typedef struct Node {
     int data;
     struct Node* left;
     struct Node* right;
-} Node;
+} Node;
+
+Node* search(Node* root, int value) {
+    if(root == NULL) {
+        return NULL;
+    }
+
+    while(root) {
+        if(value == root->data) {
+            return root;
+        }
+
+        if(value <= root->data) {
+            root = root->left;
+        }
+        else {
+            root = root->right;
+        }
+    }
+}
