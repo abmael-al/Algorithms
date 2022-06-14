984f694f8eda9f1185f88dc38a88727ccc000a13
Abmael <abmael.cont@gmail.com>
2022-06-14 14:18:34 -0300

    Basic setup

--- src/main/c/algorithms/interview-questions/linked-list/delete-middle-node.c
+++ src/main/c/algorithms/interview-questions/linked-list/delete-middle-node.c
@@ -11,3 +11,45 @@
 // lnput: the node c from the linked list a->b->c->d->e->f
 // Result: nothing is returned, but the new linked list looks like a->b->d->e->f
 
+typedef struct Node {
+    int value;
+    struct Node* next;
+} Node;
+
+Node* createNode(int value) {
+    Node* node = (Node*)malloc(sizeof(Node));
+
+    node->value = value;
+    node->next = NULL;
+
+    return node;
+}
+
+int isEmpty(Node* node) {
+    return node == NULL;
+}
+
+void push(int value, Node **head) {
+    Node* node = createNode(value);
+
+    if(isEmpty(*head)) {
+        *head = node;
+
+        return;
+    }
+
+    node->next = *head;
+    
+    *head = node;
+}
+
+void print(Node* head) {
+    printf("\n");
+
+    while(head) {
+        printf("%d ", head->value);
+        head = head->next;
+    }
+
+    printf("\n");
+}
