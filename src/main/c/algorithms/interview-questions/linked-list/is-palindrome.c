63abc96235cd84cb2eb04bb5bafe7e5cd84e28ef
Abmael <abmael.cont@gmail.com>
2022-06-15 14:31:46 -0300

    Verify if a given linked list is a palindrome

--- src/main/c/algorithms/interview-questions/linked-list/is-palindrome.c
+++ src/main/c/algorithms/interview-questions/linked-list/is-palindrome.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define PALINDROME 1
+#define NOT_PALINDROME 0
+
 typedef struct Node {
     int value;
     struct Node* next;
@@ -42,4 +45,38 @@ void print(Node* head) {
     }
 
     printf("\n");
+}
+
+int validate(Node *node1, Node *node2) {
+    return (node1->value == node2->value) ? PALINDROME : NOT_PALINDROME;
+}
+
+void isPalindromeUtil(Node *end, Node **begin, int *validator) {
+    if(isEmpty(end)) {
+        return;
+    }
+
+    isPalindromeUtil(end->next, begin, validator);
+
+    if(!(*validator)) {
+        return;
+    }
+
+    *validator = validate(end, *begin);
+
+    *begin = (*begin)->next;
+}   
+
+int isPalindrome(Node *head) {
+    if(isEmpty(head)) {
+        return NOT_PALINDROME;
+    }
+
+    int validation;
+    Node* begin = head;
+    Node* end = head;
+
+    isPalindromeUtil(end, &begin, &validation);
+
+    return validation;
 }
