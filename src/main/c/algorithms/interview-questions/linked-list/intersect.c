a7762cfab765a686ab5c89c9b9f8420256ae5775
Abmael <abmael.cont@gmail.com>
2022-06-15 14:56:31 -0300

    Basic setup

---
+++ src/main/c/algorithms/interview-questions/linked-list/intersect.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int value;
+    struct Node* next;
+} Node;
+
+Node* createNode(int value) {
+    Node* node = (Node*)malloc(sizeof(Node));
+
+    node->value = value;
+    node->next = NULL;
+
+    return node;
+}
+
+int isEmpty(Node* node) {
+    return node == NULL;
+}
+
+void push(int value, Node **head) {
+    Node* node = createNode(value);
+
+    if(isEmpty(*head)) {
+        *head = node;
+
+        return;
+    }
+
+    node->next = *head;
+    
+    *head = node;
+}
+
+void print(Node* head) {
+    printf("\n");
+
+    while(head) {
+        printf("%d ", head->value);
+        head = head->next;
+    }
+
+    printf("\n");
+}
