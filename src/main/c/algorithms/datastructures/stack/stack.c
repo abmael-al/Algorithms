3adce86d7cd2892947c356ec4221e1a012454539
Abmael <abmael.cont@gmail.com>
2022-06-16 15:18:27 -0300

    node struct & create node function

---
+++ src/main/c/algorithms/datastructures/stack/stack.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int value;
+    struct Node* next;
+} Node;
+
+Node* createNode(int value) {
+    Node* node = (Node*)malloc(sizeof(Node));
+
+    node->value = value;
+    node->next = NULL;
+
+    return node;
+}
