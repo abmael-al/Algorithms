fa766faf960fee6dbc0a866462c2b4366ad3044a
Abmael <abmael.cont@gmail.com>
2022-06-17 12:51:02 -0300

    Basic setup to solve the problem

---
+++ src/main/c/algorithms/interview-questions/stack/reverse-string.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ERR_EMPTY_STACK -1
+
+typedef struct Node {
+    char chr;
+    struct Node* next;
+} Node;
+
+Node* createNode(char chr) {
+    Node* node = (Node*)malloc(sizeof(Node));
+
+    node->chr = chr;
+    node->next = NULL;
+
+    return node;
+}
+
+int isEmpty(Node* stack) {
+    return stack == NULL;
+}
+
+char top(Node *stack) {
+    if(isEmpty(stack)) {
+        return ERR_EMPTY_STACK;
+    }
+
+    return stack->chr;
+}
+
+void push(char chr, Node **stack) {
+    Node* node = createNode(chr);
+
+    if(isEmpty(*stack)) {
+        *stack = node;
+
+        return;
+    }
+
+    node->next = *stack;
+    
+    *stack = node;
+}
+
+char pop(Node **stack) {
+    if(isEmpty(*stack)) {
+        return ERR_EMPTY_STACK;
+    }
+
+    Node* poppedNode = *stack;
+    int poppedChar = poppedNode->chr;
+
+    if(isEmpty((*stack)->next)) {
+        *stack = NULL;
+    }
+    else if(!isEmpty((*stack)->next)) {
+        *stack = (*stack)->next;
+    }
+
+    free(poppedNode);
+
+    return poppedChar;
+}
