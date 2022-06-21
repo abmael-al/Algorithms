67b5075f0afa8f25964058898e194c21009e175e
Abmael <abmael.cont@gmail.com>
2022-06-21 13:27:07 -0300

    Basic setup to solve the problem

--- src/main/c/algorithms/interview-questions/linked-list/partition.c
+++ src/main/c/algorithms/interview-questions/linked-list/partition.c
@@ -6,3 +6,56 @@
  
 // Input: 3 -> 1 -> 2 -> 10 -> 5 -> 5 -> 8 
 // Output: 3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1 [partition= 5]
+
+// Source [BOOK]: Cracking the Coding Interview. Chapter 2, page 94.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int value;
+    struct Node* next;
+} Node;
+
+Node* createNode(int value) {
+    Node* node = (Node*)malloc(sizeof(Node));
+
+    node->value = value;
+    node->next = NULL;
+
+    return node;
+}
+
+int isEmpty(Node* node) {
+    return node == NULL;
+}
+
+void push(int value, Node **head) {
+    Node* node = createNode(value);
+
+    if(isEmpty(*head)) {
+        *head = node;
+
+        return;
+    }
+
+    node->next = *head;
+    
+    *head = node;
+}
+
+void print(Node* head) {
+    printf("\n");
+
+    while(head) {
+        printf("%d ", head->value);
+        head = head->next;
+    }
+
+    printf("\n");
+}
+
+int main(){
+
+    return 0;
+}
