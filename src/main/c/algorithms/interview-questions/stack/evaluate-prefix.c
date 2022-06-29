7ed918b08e64d1d80b4ab83c6799ff6b1def79b6
Abmael <abmael.cont@gmail.com>
2022-06-29 15:26:16 -0300

    Evaluate prefix expression

--- src/main/c/algorithms/interview-questions/stack/evaluate-prefix.c
+++ src/main/c/algorithms/interview-questions/stack/evaluate-prefix.c
@@ -66,3 +66,57 @@ int isOperator(char chr) {
         || (chr == '+')
         || (chr == '-');
 }
+
+int handleOperation(const char operator, Node **stack) {
+    int result;
+
+    const int operand1 = pop(stack);
+    const int operand2 = pop(stack);
+
+    switch(operator) {
+        case '*': 
+            result = operand2 * operand1;
+            break;
+
+        case '/': 
+            result = operand2 / operand1;
+            break;
+
+        case '+': 
+            result = operand2 + operand1;
+            break;
+                
+        case '-': 
+            result = operand2 - operand1;
+            break;    
+    }
+
+    return result;
+}
+
+int evaluatePrefixExpression(const char expr[]) {
+    const size_t exprLen = strlen(expr);
+    Node *stack = NULL;
+
+    int i;
+    for(i = exprLen - 1; i >= 0; i--) {
+        const char token = expr[i];
+
+        if(isdigit(token)) {
+            int tokenToInteger = token - '0';
+            
+            push(tokenToInteger, &stack);
+        }
+        else if(isOperator(token)) {
+            int result = handleOperation(token, &stack);
+
+            push(result, &stack);
+        }
+    }
+
+    int result = pop(&stack);
+
+    free(stack);
+
+    return result;
+}
