d30be30d4fbe9ebdb29e564af565ca84616e9f7b
Abmael <abmael.cont@gmail.com>
2022-06-30 15:27:46 -0300

    Enqueue an element at a queue

--- src/main/c/algorithms/datastructures/queue/queue-with-array.c
+++ src/main/c/algorithms/datastructures/queue/queue-with-array.c
@@ -22,3 +22,17 @@ int isFull(const Queue queue) {
 int isEmpty(const Queue queue) {
     return (queue.head == -1) && (queue.tail == -1);
 }
+
+void enqueue(const double value, Queue *queue) {
+    if(isFull(*queue)) {
+        return;
+    }
+
+    if(isEmpty(*queue)) {
+        queue->head = 0;
+        queue->tail = 0;
+    }
+
+    queue->queue[queue->tail] = value;
+    queue->tail = (queue->tail + 1) % MAX_SIZE;
+}
