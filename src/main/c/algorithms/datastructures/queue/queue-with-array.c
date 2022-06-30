dfb939f2a1012a391867cb0214601b04ab11e2bf
Abmael <abmael.cont@gmail.com>
2022-06-30 15:25:09 -0300

    Initialize fields of a queue, check if a queue is full and check if a queue is empty

--- src/main/c/algorithms/datastructures/queue/queue-with-array.c
+++ src/main/c/algorithms/datastructures/queue/queue-with-array.c
@@ -2,9 +2,23 @@
 #include <stdlib.h>
 
 #define MAX_SIZE 10
+#define ERR_EMPTY_QUEUE -1
 
 typedef struct Queue {
     double queue[MAX_SIZE];
     int head;
     int tail;
 } Queue;
+
+void initQueue(Queue *queue) {
+    queue->head = -1;
+    queue->tail = -1;
+}
+
+int isFull(const Queue queue) {
+    return ((queue.tail + 1) % MAX_SIZE) == queue.head;
+}
+
+int isEmpty(const Queue queue) {
+    return (queue.head == -1) && (queue.tail == -1);
+}
