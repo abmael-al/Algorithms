8bef1a17d75d61c3c13992ccbe09b8c167d201c6
Abmael <abmael.cont@gmail.com>
2022-06-30 15:07:39 -0300

    Driver code

--- src/main/c/algorithms/datastructures/queue/queue-with-linked-list.c
+++ src/main/c/algorithms/datastructures/queue/queue-with-linked-list.c
@@ -81,3 +81,21 @@ void print(Queue queue) {
         iterator = iterator->next;
     }
 }
+
+int main() {
+    Queue queue;
+
+    initQueue(&queue);
+
+    enqueue(1, &queue);
+    enqueue(2, &queue);
+    enqueue(3, &queue);
+    enqueue(4, &queue);
+    enqueue(5, &queue);
+
+    dequeue(&queue);
+
+    print(queue);
+
+    return 0;
+}
