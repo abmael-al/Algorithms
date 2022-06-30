e24353b2ab620ab5ac38af11d72b95cfdfa2cb72
Abmael <abmael.cont@gmail.com>
2022-06-30 13:37:54 -0300

    Create node, chech if queue is empty and initialize a queue

--- src/main/c/algorithms/datastructures/queue/queue-with-linked-list.c
+++ src/main/c/algorithms/datastructures/queue/queue-with-linked-list.c
@@ -10,3 +10,21 @@ typedef struct Queue {
     Node *head;
     Node *tail;
 } Queue;
+
+Node * createNode(const double value) {
+    Node *node = (Node *)malloc(sizeof(Node));
+
+    node->value = value;
+    node->next = NULL;
+
+    return node;
+}
+
+int isEmpty(Queue queue) {
+    return queue.head == NULL;
+}
+
+void initQueue(Queue *queue) {
+    queue->head = NULL;
+    queue->tail = NULL;
+}
