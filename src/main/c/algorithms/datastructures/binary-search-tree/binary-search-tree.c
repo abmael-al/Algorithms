8686f45de9c55ae7c24fee7a224ded4141826c0a
Abmael <abmael.cont@gmail.com>
2022-07-11 13:21:25 -0300

    Insert an integer value at a binary search tree

---
+++ src/main/c/algorithms/datastructures/binary-search-tree/binary-search-tree.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int value;
+    struct Node *left;
+    struct Node *right;
+} Node;
+
+Node * createNode(const int value) {
+    Node *node = (Node *)malloc(sizeof(Node));
+
+    node->value = value;
+    node->left = NULL;
+    node->right = NULL;
+
+    return node;
+}
+
+int isEmpty(Node *node) {
+    return node == NULL;
+}
+
+void insert(const int value, Node **root) {
+    Node *node = createNode(value);
+    
+    if(isEmpty(*root)) {
+        *root = node;
+        return;
+    }
+
+    Node *current = *root;
+    Node *parent = NULL;
+
+    while(1) {
+        parent = current;
+
+        if(value < parent->value) {
+            current = current->left;
+
+            if(isEmpty(current)) {
+                parent->left = node;
+                break;
+            }
+        }
+        else {
+            current = current->right;
+
+            if(isEmpty(current)) {
+                parent->right = node;
+                break;
+            }
+        }
+    }
+}
