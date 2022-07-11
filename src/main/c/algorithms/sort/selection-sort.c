e282a8f954153ff8aecbc9576374f4cce1e6a55a
Abmael <abmael.cont@gmail.com>
2022-07-10 22:33:10 -0300

    Driver code

--- src/main/c/algorithms/sort/selection-sort.c
+++ src/main/c/algorithms/sort/selection-sort.c
@@ -22,3 +22,17 @@ void selectionSort(int array[], const size_t length) {
         array[i] = temp;
     }
 }
+
+int main() {
+    int arr[] = { 6, 34, 1, 5, 2, 8, 1, 54, 2 };
+    const size_t length = sizeof arr / sizeof(int);
+
+    selectionSort(arr, length);
+
+    int i;
+    for(i = 0; i < length; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}   
