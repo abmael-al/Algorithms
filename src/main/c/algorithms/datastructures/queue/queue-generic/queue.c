7739dc3a134f67ad6bcbfda6ccdba952be4ca5c9
Abmael <abmael.cont@gmail.com>
2022-07-18 18:34:54 -0300

    Create an initialized node

--- src/main/c/algorithms/datastructures/queue/queue-generic/queue.c
+++ src/main/c/algorithms/datastructures/queue/queue-generic/queue.c
@@ -12,3 +12,39 @@
 #define ERR_DATA_MEM_ALLOCATION_NOT_ALLOWED -3
 #define ERR_NODE_MEM_ALLOCATION_NOT_ALLOWED -4
 #define ERR_ILLEGAL_MEM_SIZE -5
+
+typedef struct Node {
+    void *data;
+    struct Node *next;
+} Node;
+
+int Node_Create(Node **dest, const size_t mem_size) {
+    if(mem_size == 0) {
+        return ERR_ILLEGAL_MEM_SIZE;
+    }
+
+    Node *node = (Node *)malloc(sizeof(Node));
+    
+    *dest = node;
+
+    if(node == NULL) {
+        return ERR_NODE_MEM_ALLOCATION_NOT_ALLOWED;
+    }
+
+    node->data = malloc(mem_size);
+    
+    if(node->data == NULL) {
+        // Errant code tends to fail faster with a 0 pointer/data 
+        // than with its original data still potentially intact. 
+        // Faster failing code is easier to debug.
+        memset(node, 0, sizeof *node);
+        free(node);
+        *dest = NULL;
+        
+        return ERR_DATA_MEM_ALLOCATION_NOT_ALLOWED;
+    }
+
+    node->next = NULL;
+
+    return ALLOCATION_ALLOWED;
+}
