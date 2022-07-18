6bc800397526155ad21447a8ae907a0c9fd45759
Abmael <abmael.cont@gmail.com>
2022-07-18 18:33:58 -0300

    Macros definitions and imports

---
+++ src/main/c/algorithms/datastructures/queue/queue-generic/queue.c
@@ -0,0 +1,14 @@
+// File header
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include "Queue.h"
+
+#define ALLOCATION_ALLOWED 2
+#define OPERATION_ALLOWED 1
+#define ERR_EMPTY_QUEUE -1
+#define ERR_MEMORY_ALLOCATION_NOT_ALLOWED -2
+#define ERR_DATA_MEM_ALLOCATION_NOT_ALLOWED -3
+#define ERR_NODE_MEM_ALLOCATION_NOT_ALLOWED -4
+#define ERR_ILLEGAL_MEM_SIZE -5
